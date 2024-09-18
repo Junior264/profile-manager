e73d36f589a7397adce5ca89b57ddb4a8ae248ee
Ewald B <ewald95@mail.ru>
2024-09-18 12:04:57 +0200

    fix(Repository): Uncomment repository

--- src/main/java/dev/ewald/profile_manager/repository/CustomerRepository.java
+++ src/main/java/dev/ewald/profile_manager/repository/CustomerRepository.java
@@ -1,18 +1,18 @@
-// package dev.ewald.profile_manager.repository;
+package dev.ewald.profile_manager.repository;
 
-// import java.util.Optional;
+import java.util.Optional;
 
-// import org.springframework.data.jpa.repository.JpaRepository;
-// import org.springframework.data.jpa.repository.Modifying;
-// import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 
-// import dev.ewald.profile_manager.model.Customer;
+import dev.ewald.profile_manager.model.Customer;
 
-// public interface CustomerRepository extends JpaRepository<Customer, Integer>{
-//     boolean existsCustomerByEmail(String email);
-//     boolean existsCustomerById(Integer id);
-//     Optional<Customer> findCustomerByEmail(String email);
-//     @Modifying(clearAutomatically = true)
-//     @Query("UPDATE Customer c SET c.profileImageId = ?1 WHERE c.id = ?2")
-//     int updateProfileImageId(String profileImageId, Integer customerId);
-// }
+public interface CustomerRepository extends JpaRepository<Customer, Integer>{
+    boolean existsCustomerByEmail(String email);
+    boolean existsCustomerById(Integer id);
+    Optional<Customer> findCustomerByEmail(String email);
+    @Modifying(clearAutomatically = true)
+    @Query("UPDATE Customer c SET c.profileImageId = ?1 WHERE c.id = ?2")
+    int updateProfileImageId(String profileImageId, Integer customerId);
+}
